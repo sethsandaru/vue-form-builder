f3806c261c79c5f38a9451063a5135c64f6f7dbd
Nico <nico.halcyondigital@gmail.com>
2020-12-04 13:10:23 +0800

    test edit
    Signed-off-by: Nico <nico.halcyondigital@gmail.com>

--- src/views/builder/FormConfiguration.vue
+++ src/views/builder/FormConfiguration.vue
@@ -2,7 +2,7 @@
     <div class="form-configuration-block pbottom-10">
         <button :class="styles.BUTTON.PRIMARY" @click="open">
             <span v-html="$form.getIcon('cog')"></span>
-            <span>Form Configurations</span>
+            <span>Form Configuration</span>
         </button>
 
     </div>
