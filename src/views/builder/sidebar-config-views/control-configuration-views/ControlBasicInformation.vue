df0ce4a3d7de35af2ab69a558f4240e2bc9b850d
Jesse Marple <jesse@ossium.co>
2021-04-09 15:38:27 -0400

    Adds groups to field mappings

--- src/views/builder/sidebar-config-views/control-configuration-views/ControlBasicInformation.vue
+++ src/views/builder/sidebar-config-views/control-configuration-views/ControlBasicInformation.vue
@@ -1,88 +1,122 @@
 <template>
-    <SidebarToggleableContainer headline="Basic Detail">
-        <div :class="styles.FORM.FORM_GROUP">
-            <label>Unique ID</label>
-            <input type="text" :class="styles.FORM.FORM_CONTROL" :value="control.uniqueId" readonly>
-        </div>
+  <SidebarToggleableContainer headline="Basic Detail">
+    <div :class="styles.FORM.FORM_GROUP">
+      <label>Unique ID</label>
+      <input
+        type="text"
+        :class="styles.FORM.FORM_CONTROL"
+        :value="control.uniqueId"
+        readonly
+      />
+    </div>
 
-        <div :class="styles.FORM.FORM_GROUP">
-            <label>Name (Must be unique)</label>
-            <input type="text" :class="styles.FORM.FORM_CONTROL" v-model="control.name" :readonly="fieldMappingSet">
-        </div>
+    <div :class="styles.FORM.FORM_GROUP">
+      <label>Name (Must be unique)</label>
+      <input
+        type="text"
+        :class="styles.FORM.FORM_CONTROL"
+        v-model="control.name"
+        :readonly="fieldMappingSet"
+      />
+    </div>
 
-        <div :class="styles.FORM.FORM_GROUP">
-            <label>Field Mapping</label>
-            <select class="form-control" @change="handleFieldMappingSelection">
-              <option value=""></option>
-              <option :value="field.field" v-for="(field, index) in fieldMappings" :key="index">{{field.name}}</option>
-            </select>
-        </div>
+    <div :class="styles.FORM.FORM_GROUP">
+      <label>Field Mapping</label>
+      <select class="form-control" @change="handleFieldMappingSelection">
+        <option value=""></option>
+        <optgroup
+          v-for="(group, index) in fieldMappings"
+          :key="index"
+          :label="group.groupName"
+        >
+          <option
+            :value="field.field"
+            v-for="(field, fieldIndex) in group"
+            :key="fieldIndex"
+            >{{ field.name }}</option
+          >
+        </optgroup>
+      </select>
+    </div>
 
-        <div :class="styles.FORM.FORM_GROUP">
-            <label>Label</label>
-            <input type="text" :class="styles.FORM.FORM_CONTROL" v-model="control.label">
-        </div>
+    <div :class="styles.FORM.FORM_GROUP">
+      <label>Label</label>
+      <input
+        type="text"
+        :class="styles.FORM.FORM_CONTROL"
+        v-model="control.label"
+      />
+    </div>
 
-        <div :class="styles.FORM.FORM_GROUP">
-            <label>Sub-label</label>
-            <input type="text" :class="styles.FORM.FORM_CONTROL" v-model="control.subLabel">
-        </div>
+    <div :class="styles.FORM.FORM_GROUP">
+      <label>Sub-label</label>
+      <input
+        type="text"
+        :class="styles.FORM.FORM_CONTROL"
+        v-model="control.subLabel"
+      />
+    </div>
 
-        <div :class="styles.FORM.FORM_GROUP">
-            <label>
-                Show Label?
-                <input type="checkbox" v-model="control.isShowLabel">
-            </label>
-        </div>
+    <div :class="styles.FORM.FORM_GROUP">
+      <label>
+        Show Label?
+        <input type="checkbox" v-model="control.isShowLabel" />
+      </label>
+    </div>
 
-        <div :class="styles.FORM.FORM_GROUP">
-            <label>Field Placeholder (Optional)</label>
-            <input type="text" :class="styles.FORM.FORM_CONTROL" v-model="control.placeholderText">
-        </div>
+    <div :class="styles.FORM.FORM_GROUP">
+      <label>Field Placeholder (Optional)</label>
+      <input
+        type="text"
+        :class="styles.FORM.FORM_CONTROL"
+        v-model="control.placeholderText"
+      />
+    </div>
 
-        <div :class="styles.FORM.FORM_GROUP">
-            <label>Default Value (Optional)</label>
-            <input type="text" :class="styles.FORM.FORM_CONTROL" v-model="control.defaultValue">
-        </div>
-
-    </SidebarToggleableContainer>
+    <div :class="styles.FORM.FORM_GROUP">
+      <label>Default Value (Optional)</label>
+      <input
+        type="text"
+        :class="styles.FORM.FORM_CONTROL"
+        v-model="control.defaultValue"
+      />
+    </div>
+  </SidebarToggleableContainer>
 </template>
 
 <script>
-    import SidebarToggleableContainer from "@/views/container-views/SidebarToggleableContainer";
-    import {STYLE_INJECTION_MIXIN} from "@/mixins/style-injection-mixin";
-    import {FIELD_MAPPINGS_INJECTION_MIXIN} from "@/mixins/field-mappings-injection-mixin"
+import SidebarToggleableContainer from "@/views/container-views/SidebarToggleableContainer";
+import { STYLE_INJECTION_MIXIN } from "@/mixins/style-injection-mixin";
+import { FIELD_MAPPINGS_INJECTION_MIXIN } from "@/mixins/field-mappings-injection-mixin";
 
-    export default {
-        name: "ControlBasicInformation",
-        mixins: [STYLE_INJECTION_MIXIN, FIELD_MAPPINGS_INJECTION_MIXIN],
-        components: {SidebarToggleableContainer},
-        props: {
-            control: Object,
-        },
-        data() {
-          return {
-            fieldMappingSet: false
-          }
-        },
-        methods: {
-          handleFieldMappingSelection(event){
-            let value = event.target.value
-            if(value){
-              this.fieldMappingSet = true
-              this.control.name = value
-              return
-            }
+export default {
+  name: "ControlBasicInformation",
+  mixins: [STYLE_INJECTION_MIXIN, FIELD_MAPPINGS_INJECTION_MIXIN],
+  components: { SidebarToggleableContainer },
+  props: {
+    control: Object
+  },
+  data() {
+    return {
+      fieldMappingSet: false
+    };
+  },
+  methods: {
+    handleFieldMappingSelection(event) {
+      let value = event.target.value;
+      if (value) {
+        this.fieldMappingSet = true;
+        this.control.name = value;
+        return;
+      }
 
-            this.fieldMappingSet = false
-            this.control.name = ""
-            return
-          }
-        },
-        mounted() {},
+      this.fieldMappingSet = false;
+      this.control.name = "";
+      return;
     }
+  },
+  mounted() {}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
