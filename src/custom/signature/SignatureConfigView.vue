bb11f1dcd1a617d04db7820005a5553cdefaeffd
Jesse Marple <jesse@ossium.co>
2021-06-16 14:59:06 -0400

    Add redemo control to test custom required props

---
+++ src/custom/signature/SignatureConfigView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!---
+        You can use the `control` object - basically, it's main data of your custom control field
+        Because I had 2 more specific config data (isChecked & switchColor)
+        So I added 2 more fields below to config it.
+        --->
+    <div>This control has no configuration.</div>
+  </div>
+</template>
+
+<script>
+import { BaseControlConfigSkeleton } from "v-form-builder";
+
+export default {
+  name: "SignatureConfigView",
+  extends: BaseControlConfigSkeleton
+};
+</script>
---
+++ src/custom/signature/SignatureControl.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div style="margin-bottom: .45rem;">
+      Enter your name for a digital signature
+    </div>
+    <input
+      :id="control.uniqueId"
+      type="text"
+      :class="controlFieldClass"
+      :value="value"
+      :name="control.name || control.uniqueId"
+      :placeholder="control.placeholderText"
+      @input="updateValue($event.target.value)"
+      style="font-family: cursive; "
+    />
+  </div>
+</template>
+
+<script>
+import { BaseControlSkeleton } from "v-form-builder";
+
+export default {
+  name: "SignatureControl",
+  extends: BaseControlSkeleton,
+  data: () => ({}),
+
+  mounted() {}
+};
+</script>
